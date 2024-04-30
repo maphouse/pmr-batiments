3760d01e8ac786573e65465dc40860c68c9910fb
maphouse <eehmshaw@gmail.com>
2024-04-30 01:40:50 -0400

    map livrable

--- layers/layers.js
+++ layers/layers.js
@@ -1,38 +1,66 @@
 var wms_layers = [];
 
-var format_Batiments_0 = new ol.format.GeoJSON();
-var features_Batiments_0 = format_Batiments_0.readFeatures(json_Batiments_0, 
+
+        var lyr_OSMStandard_0 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var format_Annedeconstruction_1 = new ol.format.GeoJSON();
+var features_Annedeconstruction_1 = format_Annedeconstruction_1.readFeatures(json_Annedeconstruction_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_Batiments_0 = new ol.source.Vector({
+var jsonSource_Annedeconstruction_1 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_Batiments_0.addFeatures(features_Batiments_0);
-var lyr_Batiments_0 = new ol.layer.Vector({
+jsonSource_Annedeconstruction_1.addFeatures(features_Annedeconstruction_1);
+var lyr_Annedeconstruction_1 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_Batiments_0, 
-                style: style_Batiments_0,
-                popuplayertitle: "Batiments",
+                source:jsonSource_Annedeconstruction_1, 
+                style: style_Annedeconstruction_1,
+                popuplayertitle: "Unité d'évaluation foncière",
                 interactive: true,
-    title: 'Batiments<br />\
-    <img src="styles/legend/Batiments_0_0.png" /> - 1866<br />\
-    <img src="styles/legend/Batiments_0_1.png" /> 1866 - 1879<br />\
-    <img src="styles/legend/Batiments_0_2.png" /> 1880 - 1899<br />\
-    <img src="styles/legend/Batiments_0_3.png" /> 1900 - 1917<br />\
-    <img src="styles/legend/Batiments_0_4.png" /> 1918 - 1934<br />\
-    <img src="styles/legend/Batiments_0_5.png" /> 1935 - 1943<br />\
-    <img src="styles/legend/Batiments_0_6.png" /> 1944 - 1959<br />\
-    <img src="styles/legend/Batiments_0_7.png" /> 1960 - 2024<br />'
+    title: 'Année de construction<br />\
+    <img src="styles/legend/Annedeconstruction_1_0.png" /> --------- 1866<br />\
+    <img src="styles/legend/Annedeconstruction_1_1.png" /> 1866 - 1879<br />\
+    <img src="styles/legend/Annedeconstruction_1_2.png" /> 1880 - 1899<br />\
+    <img src="styles/legend/Annedeconstruction_1_3.png" /> 1900 - 1917<br />\
+    <img src="styles/legend/Annedeconstruction_1_4.png" /> 1918 - 1934<br />\
+    <img src="styles/legend/Annedeconstruction_1_5.png" /> 1935 - 1943<br />\
+    <img src="styles/legend/Annedeconstruction_1_6.png" /> 1944 - 1959<br />\
+    <img src="styles/legend/Annedeconstruction_1_7.png" /> 1960 - 2024<br />'
         });
-var group_frame = new ol.layer.Group({
-                                layers: [lyr_Batiments_0,],
+var group_Unitsdvaluationfoncire = new ol.layer.Group({
+                                layers: [lyr_Annedeconstruction_1,],
                                 fold: "open",
-                                title: "frame"});
+                                title: "Unités d'évaluation foncière"});
+var group_misc = new ol.layer.Group({
+                                layers: [],
+                                fold: "open",
+                                title: "misc"});
+var group_DAs = new ol.layer.Group({
+                                layers: [],
+                                fold: "open",
+                                title: "DAs"});
+var group_stm = new ol.layer.Group({
+                                layers: [],
+                                fold: "open",
+                                title: "stm"});
+var group_basemap = new ol.layer.Group({
+                                layers: [],
+                                fold: "open",
+                                title: "basemap"});
 
-lyr_Batiments_0.setVisible(true);
-var layersList = [group_frame];
-lyr_Batiments_0.set('fieldAliases', {'fid': 'fid', 'ID_UEV': 'ID_UEV', 'CIVIQUE_DE': 'CIVIQUE_DE', 'CIVIQUE_FI': 'CIVIQUE_FI', 'NOM_RUE': 'NOM_RUE', 'SUITE_DEBU': 'SUITE_DEBU', 'MUNICIPALI': 'MUNICIPALI', 'ETAGE_HORS': 'ETAGE_HORS', 'NOMBRE_LOG': 'NOMBRE_LOG', 'ANNEE_CONS': 'ANNEE_CONS', 'CODE_UTILI': 'CODE_UTILI', 'LETTRE_DEB': 'LETTRE_DEB', 'LETTRE_FIN': 'LETTRE_FIN', 'LIBELLE_UT': 'LIBELLE_UT', 'CATEGORIE_': 'CATEGORIE_', 'MATRICULE8': 'MATRICULE8', 'SUPERFICIE': 'SUPERFICIE', 'SUPERFIC_1': 'SUPERFIC_1', 'NO_ARROND_': 'NO_ARROND_', });
-lyr_Batiments_0.set('fieldImages', {'fid': 'TextEdit', 'ID_UEV': 'TextEdit', 'CIVIQUE_DE': 'TextEdit', 'CIVIQUE_FI': 'TextEdit', 'NOM_RUE': 'TextEdit', 'SUITE_DEBU': 'TextEdit', 'MUNICIPALI': 'TextEdit', 'ETAGE_HORS': 'TextEdit', 'NOMBRE_LOG': 'TextEdit', 'ANNEE_CONS': 'TextEdit', 'CODE_UTILI': 'TextEdit', 'LETTRE_DEB': 'TextEdit', 'LETTRE_FIN': 'TextEdit', 'LIBELLE_UT': 'TextEdit', 'CATEGORIE_': 'TextEdit', 'MATRICULE8': 'TextEdit', 'SUPERFICIE': 'TextEdit', 'SUPERFIC_1': 'TextEdit', 'NO_ARROND_': 'TextEdit', });
-lyr_Batiments_0.set('fieldLabels', {'fid': 'no label', 'ID_UEV': 'no label', 'CIVIQUE_DE': 'no label', 'CIVIQUE_FI': 'no label', 'NOM_RUE': 'no label', 'SUITE_DEBU': 'no label', 'MUNICIPALI': 'no label', 'ETAGE_HORS': 'no label', 'NOMBRE_LOG': 'no label', 'ANNEE_CONS': 'no label', 'CODE_UTILI': 'no label', 'LETTRE_DEB': 'no label', 'LETTRE_FIN': 'no label', 'LIBELLE_UT': 'no label', 'CATEGORIE_': 'no label', 'MATRICULE8': 'no label', 'SUPERFICIE': 'no label', 'SUPERFIC_1': 'no label', 'NO_ARROND_': 'no label', });
-lyr_Batiments_0.on('precompose', function(evt) {
+lyr_OSMStandard_0.setVisible(true);lyr_Annedeconstruction_1.setVisible(true);
+var layersList = [lyr_OSMStandard_0,group_Unitsdvaluationfoncire];
+lyr_Annedeconstruction_1.set('fieldAliases', {'fid': 'fid', 'ID_UEV': 'ID_UEV', 'CIVIQUE_DE': 'CIVIQUE_DE', 'CIVIQUE_FI': 'CIVIQUE_FI', 'NOM_RUE': 'NOM_RUE', 'SUITE_DEBU': 'SUITE_DEBU', 'MUNICIPALI': 'MUNICIPALI', 'ETAGE_HORS': 'ETAGE_HORS', 'NOMBRE_LOG': 'NOMBRE_LOG', 'ANNEE_CONS': 'ANNEE_CONS', 'CODE_UTILI': 'CODE_UTILI', 'LETTRE_DEB': 'LETTRE_DEB', 'LETTRE_FIN': 'LETTRE_FIN', 'LIBELLE_UT': 'LIBELLE_UT', 'CATEGORIE_': 'CATEGORIE_', 'MATRICULE8': 'MATRICULE8', 'SUPERFICIE': 'SUPERFICIE', 'SUPERFIC_1': 'SUPERFIC_1', 'NO_ARROND_': 'NO_ARROND_', });
+lyr_Annedeconstruction_1.set('fieldImages', {'fid': 'TextEdit', 'ID_UEV': 'Hidden', 'CIVIQUE_DE': 'TextEdit', 'CIVIQUE_FI': 'TextEdit', 'NOM_RUE': 'TextEdit', 'SUITE_DEBU': 'TextEdit', 'MUNICIPALI': 'Hidden', 'ETAGE_HORS': 'TextEdit', 'NOMBRE_LOG': 'TextEdit', 'ANNEE_CONS': 'TextEdit', 'CODE_UTILI': 'Hidden', 'LETTRE_DEB': 'Hidden', 'LETTRE_FIN': 'Hidden', 'LIBELLE_UT': 'TextEdit', 'CATEGORIE_': 'TextEdit', 'MATRICULE8': 'TextEdit', 'SUPERFICIE': 'TextEdit', 'SUPERFIC_1': 'TextEdit', 'NO_ARROND_': 'Hidden', });
+lyr_Annedeconstruction_1.set('fieldLabels', {'fid': 'inline label - always visible', 'CIVIQUE_DE': 'inline label - always visible', 'CIVIQUE_FI': 'inline label - always visible', 'NOM_RUE': 'inline label - always visible', 'SUITE_DEBU': 'inline label - always visible', 'ETAGE_HORS': 'inline label - always visible', 'NOMBRE_LOG': 'inline label - always visible', 'ANNEE_CONS': 'inline label - always visible', 'LIBELLE_UT': 'inline label - always visible', 'CATEGORIE_': 'inline label - always visible', 'MATRICULE8': 'inline label - always visible', 'SUPERFICIE': 'inline label - always visible', 'SUPERFIC_1': 'inline label - always visible', });
+lyr_Annedeconstruction_1.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
---
+++ styles/Annedeconstruction_1_style.js
@@ -0,0 +1,82 @@
+var size = 0;
+var placement = 'point';
+
+var style_Annedeconstruction_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("ANNEE_CONS");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 0.000000 && value <= 1865.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.22799999999999998}),fill: new ol.style.Fill({color: 'rgba(122,4,3,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 1866.000000 && value <= 1879.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.22799999999999998}),fill: new ol.style.Fill({color: 'rgba(217,56,7,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 1880.000000 && value <= 1899.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.22799999999999998}),fill: new ol.style.Fill({color: 'rgba(254,153,44,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 1900.000000 && value <= 1917.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.22799999999999998}),fill: new ol.style.Fill({color: 'rgba(211,232,53,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 1918.000000 && value <= 1934.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.22799999999999998}),fill: new ol.style.Fill({color: 'rgba(100,253,106,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 1935.000000 && value <= 1943.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.22799999999999998}),fill: new ol.style.Fill({color: 'rgba(27,208,213,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 1944.000000 && value <= 1959.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.22799999999999998}),fill: new ol.style.Fill({color: 'rgba(71,119,239,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 1960.000000 && value <= 2024.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.22799999999999998}),fill: new ol.style.Fill({color: 'rgba(193,193,193,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
